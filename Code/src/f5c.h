921959cd74cb0dbbec9e779da32456d425ec4394
imsuneth <e15316@eng.pdn.ac.lk>
2020-10-18 00:09:34 +0530

    align.cl successfully compiled

--- Code/src/f5c.h
+++ Code/src/f5c.h
@@ -108,14 +108,14 @@ typedef struct {
     float stdv;
     //int32_t pos;   //todo : always -1 can be removed
     //int32_t state; //todo : always -1 can be removed
-} event_t;
+} event1_t;
 
 // event table : adapted from scrappie
 typedef struct {
     size_t n;     //todo : int32_t not enough?
     size_t start; //todo : always 0?
     size_t end;   //todo : always equal to n?
-    event_t* event;
+    event1_t* event;
 } event_table;
 
 
